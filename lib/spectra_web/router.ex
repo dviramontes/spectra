1dc79c0f84b45b2bec2f75f5b2e9860b0d352d8a
David Viramontes <dviramontes@gmail.com>
2023-04-16 16:20:28 -0400

    checkpoint: 1, adds Hosting context and player schema

--- lib/spectra_web/router.ex
+++ lib/spectra_web/router.ex
@@ -18,6 +18,14 @@ defmodule SpectraWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    live "/players", PlayerLive.Index, :index
+    live "/players/new", PlayerLive.Index, :new
+    live "/players/:id/edit", PlayerLive.Index, :edit
+
+    live "/players/:id", PlayerLive.Show, :show
+    live "/players/:id/show/edit", PlayerLive.Show, :edit
+
   end
 
   # Other scopes may use custom stacks.
